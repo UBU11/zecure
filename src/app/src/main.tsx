ede4b157a017c2fd7b694fe0b6f3f55b61300ff6
UBU <uvaisubu11@gmail.com>
2026-03-04 03:02:42 +0530

    sentry initialized

--- src/app/src/main.tsx
+++ src/app/src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { ClerkProvider } from '@clerk/clerk-react'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://1fcfe6355385e94955046868e4e14b24@o4510851763732480.ingest.de.sentry.io/4510851771465808",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+});
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
